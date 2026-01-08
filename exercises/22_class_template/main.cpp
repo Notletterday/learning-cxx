ee5c1c87f63d3ab53d5fedfeffbc9e3233aeaad2
Notletterday <1305638814@qq.com>
2026-01-08 17:49:05 +0800

    完成作业

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+	for(int i = 0;i < 4;i++){
+	shape[i] = shape_[i];
+	size *= shape[i];
+	}
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,38 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+         unsigned int total_elements = 1;
+        for (int i = 0; i < 4; ++i) {
+            total_elements *= shape[i];
+        }
+        
+        unsigned int other_total_elements = 1;
+        for (int i = 0; i < 4; ++i) {
+            other_total_elements *= others.shape[i];
+        }
+        
+        for (unsigned int i = 0; i < total_elements; ++i) {
+            unsigned int idx = i;
+            unsigned int coords[4];
+            
+            for (int dim = 3; dim >= 0; --dim) {
+                coords[dim] = idx % shape[dim];
+                idx /= shape[dim];
+            }
+            
+            unsigned int other_idx = 0;
+            unsigned int stride = 1;
+            
+            for (int dim = 3; dim >= 0; --dim) {
+                unsigned int other_coord = (others.shape[dim] == 1) ? 0 : coords[dim];
+                other_idx += other_coord * stride;
+                stride *= others.shape[dim];
+            }
+            
+            if (other_idx < other_total_elements) {
+                data[i] += others.data[other_idx];
+            }
+        }
         return *this;
     }
 };
